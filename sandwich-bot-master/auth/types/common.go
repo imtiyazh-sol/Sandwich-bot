8bda7e04a1d4943987ddca601a31d3f3af4e79f7
imtiyazh-sol <imtiyaz.h@solulab.co>
2024-11-06 22:36:24 +0530

    Delete sandwich-bot-master directory

--- sandwich-bot-master/auth/types/common.go
+++
@@ -1,5 +0,0 @@
-package types
-
-type UserRequiredAssociationType struct {
-	UserID *uint `json:"user_id" validate:"required,omitempty"`
-}
--- sandwich-bot-master/telegram/utils/errors.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "errors"
-
-var (
-	ErrAccessRequired = errors.New("")
-)
